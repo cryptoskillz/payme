a28df07634ed94b8e3e0beddb9ad6d498c75854e
chris <cryptoskillz@chriss-MacBook-Air.local>
2022-06-13 02:44:51 +0700

    clean up

--- cloudflare_workers/checkpayment/src/index.js
+++ cloudflare_workers/checkpayment/src/index.js
@@ -1,12 +1,18 @@
-/**
- */
-addEventListener('scheduled', event => {
-  event.waitUntil(triggerEvent(event.scheduledTime));
-});
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
 
+async function handleRequest(request) {
+ const { readable, writable } = new TransformStream();
+  //const encoder = new TextEncoder();
+  //const writer = writable.getWriter();
+  let i=0;
+  setInterval(() => {
+    //writer.write(encoder.encode(`data: hello\n\n`));
+    console.log('hhh'+i)
+    //readable="iiii"
+    i++
+  }, 5000);
 
-async function triggerEvent(scheduledTime) {
-  // Fetch some data
-  // Update API
-  console.log('cron processed');
+  return new Response(readable);
 }
