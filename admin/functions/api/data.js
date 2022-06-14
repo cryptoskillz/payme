3fa0ddf905699a248b4701452a018b97b42250bb
chris <cryptoskillz@chriss-MacBook-Air.local>
2022-06-15 00:21:22 +0700

    render the post object dynamically

--- admin/functions/api/data.js
+++ admin/functions/api/data.js
@@ -12,8 +12,8 @@ let payLoad;
 let contentType;
 //set data main to whatever is in env for consistency
 const datamain = "data";
-let dataSchema = { id: "", paymentAddress:"",name: "", amount: "", paid: "", createdAt: "" }
-
+let dataSchema = { id: "", createdAt: "", elementData: [], paidData: [] }
+let paidDataSchema = { paidAddress: "", confirmations: 0, amount: 0 }
 //JWT model
 const jwt = require('@tsndr/cloudflare-worker-jwt');
 //unique uid module
@@ -61,17 +61,22 @@ export async function onRequestPut(context) {
 
         //set up the kv data
         const KV = context.env.kvdata;
-                let user = await KV.get("user" + details.payload.username);
+        let user = await KV.get("user" + details.payload.username);
         user = JSON.parse(user)
         //get the item
-        let theItem = await KV.get(datamain  +"-"+user.user.secret+ "]" + payLoad.id);
+        let theItem = await KV.get(datamain + "-" + user.user.secret + "]" + payLoad.id);
         //console.log(datamain+  payLoad.oldname+"]" +payLoad.id)
         //parse it
         theItem = JSON.parse(theItem)
         //console.log(theItem)
         //check that they sent up the data
         //note : we could make this simplier by just parsing the payload array.
+
+        //let elementSchema = {"type":"input","required":false,"placeholder":"","value":""}
+
         if (theItem != null) {
+            let tmp = elementSchema;
+            let value = "";
             if (payLoad.name != undefined)
                 theItem.name = payLoad.name;
             if (payLoad.paymentAddress != undefined)
@@ -85,7 +90,7 @@ export async function onRequestPut(context) {
             //await KV.delete(datamain + payLoad.oldname + "]" + payLoad.id);
             //put the new one.
             //console.log(datamain  +"-"+user.user.secret+  payLoad.id)
-            await KV.put(datamain  +"-"+user.user.secret + "]"+payLoad.id, JSON.stringify(theItem));
+            await KV.put(datamain + "-" + user.user.secret + "]" + payLoad.id, JSON.stringify(theItem));
             return new Response(JSON.stringify({ message: "Item updated", data: JSON.stringify(theItem) }), { status: 200 });
         } else
             return new Response(JSON.stringify({ error: "item not found" }), { status: 400 });
@@ -121,7 +126,7 @@ export async function onRequestDelete(context) {
         user = JSON.parse(user)
         //console.log(payLoad)
         //console.log(datamain  "]" + payLoad.deleteid)
-        await KV.delete(datamain+"-"+user.user.secret+ "]" + payLoad.deleteid);
+        await KV.delete(datamain + "-" + user.user.secret + "]" + payLoad.deleteid);
         return new Response(JSON.stringify({ message: "item deleted" }), { status: 200 });
     }
 }
@@ -137,6 +142,7 @@ export async function onRequestPost(context) {
 
     let payLoad;
     const contentType = request.headers.get('content-type')
+
     if (contentType != null) {
         //get the login credentials
         payLoad = await request.json();
@@ -144,64 +150,67 @@ export async function onRequestPost(context) {
     }
     //decode jwt
     let details = await decodeJwt(request.headers, env.SECRET)
-        const KV = context.env.kvdata;
-        //get user
+    const KV = context.env.kvdata;
+    //get user
     let user = await KV.get("user" + details.payload.username);
     user = JSON.parse(user)
-    let theCheck = await KV.list({ prefix: datamain+"-"+user.user.secret  });
+    let theCheck = await KV.list({ prefix: datamain + "-" + user.user.secret });
     let exists = 0;
+    //generate a id
     let id = uuid.v4();
+    //loop through the KV items 
     if (theCheck.keys.length > 0) {
         for (var i = 0; i < theCheck.keys.length; ++i) {
+            //build the kv object we are trying to write
             let tmp = theCheck.keys[i].name.split(']')
-            //console.log(datamain + id)
-            //console.log(tmp[0])
+            //check if matches the one we want to create
             if (tmp[0] == datamain + id)
                 exists = 1;
         }
     }
-
+    //it exists so return as we don't want to overwite do we? DO WE???
     if (exists == 1)
         return new Response(JSON.stringify({ error: "data name already exists" }), { status: 400 });
     else {
-        //alternate key method
-        //let projects = await KV.list({ prefix: "projects" + details.username + "*" });
-        //console.log(projects.keys.length)
-        //let projectsData = {data: []}
-        //let id = projects.keys.length+1
-
-        
-        let schemaJson = {
-            "fields": "",
-            "originalfields": ""
-        }
+        //get the current date
         let fDate = getDate()
-        let theData = dataSchema;
-        theData.id = id;
-        theData.name  = payLoad.name;
-        theData.amount = payLoad.amount;
-        theData.paid = payLoad.paid;
-        theData.createdAt = fDate;
-        theData.paymentAddress = payLoad.paymentAddress
-        //console.log(theData)
-        //console.log(datamain + payLoad.name + "]" + id)
-        await KV.put(datamain+"-"+user.user.secret+ "]" + id, JSON.stringify(theData));
-        //update the payment queue
+        //get the keys
+        let keys = Object.keys(payLoad);
+        //get the values
+        let values = Object.values(payLoad);
+        //set the data array schema
+        //note : We don't really have to do this but it keeps things consistent.
+        let theDataArray = dataSchema; 
+        //set the created date
+        theDataArray.createdAt = fDate;
+        //set the kv id
+        theDataArray.id = id;
+        let tmpArray =  []
+        //build the form  objects
+        for (var i = 0; i < keys.length; ++i) {
+            tmpArray.push({ "type": "input", "required": false, "placeholder": "Please enter "+keys[i]+" value", "value": ""+values[i]+"", "name": ""+keys[i]+"" })
+            //^works but is ugly.  Javascript why do you behave the way you do!!!
+        }
+        //add it to the data array
+        theDataArray.elementData = tmpArray
+        //put the KV object
+        await KV.put(datamain + "-" + user.user.secret + "]" + id, JSON.stringify(theDataArray));
+        //get the payment queue
         let queueData = await KV.get("paymentqueue");
         let paymentQueueArray = [];
-        //console.log(queueData)
-        if (queueData == null)
-        {
-            let tmp = {"kv":""}
-            tmp.kv =`${datamain}-${user.user.secret}]${id}`
+        //check  it exists (maybe redundant but costs nothing to have)
+        if (queueData == null) {
+            //create a queue and add the data
+            let tmp = { "kv": "" }
+            tmp.kv = `${datamain}-${user.user.secret}]${id}`
+            //create the first entry
             paymentQueueArray.push(tmp)
-            console.log(paymentQueueArray)
+            //put the payment queue object
             await KV.put("paymentqueue", JSON.stringify(paymentQueueArray));
-        }
-        else
-        {
-            let tmp = {"kv":""}
-            tmp.kv =`${datamain}-${user.user.secret}]${id}`
+        } else {
+            //add to the payment queue 
+            let tmp = { "kv": "" }
+            tmp.kv = `${datamain}-${user.user.secret}]${id}`
             queueData = JSON.parse(queueData)
             queueData.push(tmp)
             /*
@@ -210,11 +219,11 @@ export async function onRequestPost(context) {
                 console.log(queueData[i].kv)
             }
             */
-
-            await KV.put("paymentqueue",JSON.stringify(queueData));
+            //put the payment queue object
+            await KV.put("paymentqueue", JSON.stringify(queueData));
         }
-
-        return new Response(JSON.stringify({ message: "Item added", data: JSON.stringify(theData) }), { status: 200 });
+        //sedt lowe
+        return new Response(JSON.stringify({ message: "Item added", data: JSON.stringify(theDataArray) }), { status: 200 });
 
     }
 }
@@ -238,22 +247,22 @@ export async function onRequestGet(context) {
     let user = await KV.get("user" + details.payload.username);
     user = JSON.parse(user)
     //get the projects based on the name
-    let theData = await KV.list({ prefix: datamain+"-"+user.user.secret  });
-    let theDataArray = { data: [] }
+    let theData = await KV.list({ prefix: datamain + "-" + user.user.secret });
+    let theDataArray = []
     if ((dataid != null) && (dataid != "")) {
-        let pData = await KV.get(datamain+"-"+user.user.secret + dataid);
+        let pData = await KV.get(datamain + "-" + user.user.secret + dataid);
         theDataArray.data.push(JSON.parse(pData))
     } else {
         if (theData.keys.length > 0) {
             for (var i = 0; i < theData.keys.length; ++i) {
                 //get the item
                 let pData = await KV.get(theData.keys[i].name);
-                pData = JSON.parse(pData)
-                //console.log(pData)
+                //pData = JSON.parse(pData)
+                console.log(pData)
                 //debug for easy clean up
                 //console.log(theData.keys[i].name);
                 //await KV.delete(theData.keys[i].name);
-                theDataArray.data.push(pData)
+                theDataArray.push(pData)
             }
         }
     }
